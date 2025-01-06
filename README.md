b55ebf8a2bfac63a6192031d645ca9e8028f34bd
jramma <jrmasip097@gmail.com>
2025-01-06 12:53:33 +0100

    tailwind

---
+++ README.md
@@ -0,0 +1,1 @@
+Este proyecto es una versión del repositorio [jornadas-seguridad-informatica](https://github.com/jramma/jornadas-seguridad-informatica) pero utilizando Tailwind CSS.
---
+++ _tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#f472b6',
+        secondary: '#a3e635',
+        terciary: '#34d399',
+        accent: '#4ade80',
+        background: '#ffffff',
+        white: '#f1f1f1',
+        black: '#0f0f0f',
+        text: '#000000',
+        green: '#10b981',
+        uoc: '#00046A',
+        border: '#000000',
+      },
+      borderRadius: {
+        DEFAULT: '8px',
+      },
+      spacing: {
+        unit: '1rem',
+      },
+      fontFamily: {
+        main: ['Poppins', 'sans-serif'],
+        serif: ['dm serif text', 'serif'],
+        righteous: ['Righteous', 'sans-serif'],
+      },
+      boxShadow: {
+        card: '7px 0 0 rgb(0 0 0 / 100%)',
+      },
+      scale: {
+        'card-hover': '1.05',
+      },
+    },
+  },
+  plugins: [],
+};
